09720ab85e6518edd27072af62f15d99cbc6a67c
jgu1 <Jialiang.Gu@ucsf.edu>
2019-03-09 12:18:30 -0800

    add web feature

---
+++ flaskr.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash, jsonify, Response
+from db_classes import DAO
+import pdb
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return DAO()
+
+@app.before_request
+def before_request():
+    g.dao = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    dao = getattr(g, 'dao', None)
+    if dao is not None:
+        dao.db.close()
+
+@app.route('/')
+def show_index_page():
+    dao = getattr(g, 'dao', None)
+    type_calorie_list = dao.get_bread_calorie()
+    return render_template('index.html',type_calorie_list = type_calorie_list)
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=60000,threaded=True)
